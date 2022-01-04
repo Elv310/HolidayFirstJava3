42f0f73196d25e06202d93cabb0de81eab36fb5a
Elv310 <Elvisa310@gmail.com>
2022-01-03 19:31:34 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        for (int i = 6; i < 0; i++) {
+//
+//
+//            while (i > 0){
+//                System.out.println("HelloWorld" + i);
+//            }
+//
+//        }
+//        Scanner scanner = new Scanner(System.in);
+//        String input = "";
+//        while (!input.equals("quit")){
+//            System.out.println("Input: ");
+//            input = scanner.next().toLowerCase();
+//            System.out.println(input);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+
